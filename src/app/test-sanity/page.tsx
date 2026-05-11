8192681a56ed44cf059d397de2d7645d1773dc48
sajithks31 <sajith@digitalfarm.ae>
2026-05-11 13:45:04 +0400

    Adding Sanity debug page to troubleshoot broken images

---
+++ src/app/test-sanity/page.tsx
@@ -0,0 +1,25 @@
+import { getProjectsData } from "@/sanity/queries";
+
+export default async function TestSanityPage() {
+  const projects = await getProjectsData();
+
+  return (
+    <div style={{ padding: "50px", background: "white", color: "black" }}>
+      <h1>Sanity Data Debug</h1>
+      <pre>{JSON.stringify(projects, null, 2)}</pre>
+      <hr />
+      {projects.map((p: any) => (
+        <div key={p._id} style={{ marginBottom: "20px", border: "1px solid #ccc", padding: "10px" }}>
+          <h3>{p.title}</h3>
+          <p>ImageUrl from query: {p.imageUrl || "NULL"}</p>
+          <img 
+            src={p.imageUrl} 
+            alt="from query" 
+            style={{ width: "200px", border: "2px solid red" }} 
+            onError={(e) => console.log("Image failed:", p.title)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
